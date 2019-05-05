230932f0019575a6bd03e7195ececb810d2a58c0
“karthikGcan” <karthikganeshan.rmd@gmail.com>
2019-05-05 15:56:30 +0530

    Stack

--- stack.py
+++ stack.py
@@ -1,21 +1,56 @@
+#class Stack:
+
+    #def __init__(self):
+        #self.items = []
+    
+    #def is_empty(self):
+        #return self.items == []
+    
+    #def push(self,item,max=5):
+         #self.items.append(item)
+
+    #def  pop(self):
+        #return self.items.pop()
+
+    #def peek(self):
+        #return self.items[len(self.items)-1]
+
+
+    #def size(self):
+        #return len(self.items)
+
 class Stack:
-
-    def __init__(self):
-        self.items = []
-    
-    def is_empty(self):
-        return self.items == []
-    
-    def push(self,item,max=5):
-         self.items.append(item)
-
-    def  pop(self):
-        return self.items.pop()
+  def __init__(self):
+    self.stack = list()
+	
+  def push(self, value):
+    if value == None:
+      return False
+    else:
+      self.stack.insert(0, value)
+    return True
+	
+  def peek(self):
+    return self.stack[0]
+	
+  def pop(self):
+    if self.stack:
+      self.stack.pop(0)
+      return True
+    else:
+      return False
+	
+  def size(self):
+    return len(self.stack)
+	
+  def has(self, value):
+    if value in self.stack:
+      return True
+    else:
+      return False
+	
+  def clear(self):
+    self.stack = list()
         
 
-    def peek(self):
-        return self.items[len(self.items)-1]
-
-
-    def size(self):
-        return len(self.items)
+   
--- test_stack.py
+++ test_stack.py
@@ -1,23 +1,70 @@
-import stack
-import pytest
+#import stack
+#import pytest
 
 
-def test_push():
-    s = stack.Stack()
-    s.push(5)
-    s.push(5)
-    s.push(5)
-    s.push(5)
-    s.push(5)
-    s.push(5)
-    s.push(5)
+#def test_push():
+    #s = stack.Stack()
+    #s.push(5)
+    #s.push(5)
+    #s.push(5)
+    #s.push(5)
+    #s.push(5)
+    #s.push(5)
+    #s.push(5)
    
     #s.pop()
     #with pytest.raises(IndexError):
         #s = stack.Stack(maxsize=5)
         #assert s.pop == 0
     
-    
+import stack
+import unittest
+
+class TestStack(unittest.TestCase):
+  def testPush(self):
+    s = stack.Stack()
+    for value in range(1,101):
+      self.assertEqual(True, s.push(value))
+		
+  def testPop(self):
+    s = stack.Stack()
+    for value in range(1, 11):
+      self.assertEqual(True, s.push(value))
+      self.assertEqual(True, s.pop())
+		
+    self.assertEqual(False, s.pop())
+	
+  def testPeek(self):
+    s = stack.Stack()
+    for value in range(1, 11):
+      self.assertEqual(True, s.push(value))
+      self.assertEqual(value, s.peek())
+	
+  def testHas(self):
+    s = stack.Stack()
+    for value in range(1, 11):
+      self.assertEqual(True, s.push(value))
+      self.assertEqual(True, s.has(value))
+      self.assertEqual(False, s.has(value+1))
+	
+  def testSize(self):
+    s = stack.Stack()
+    for value in range(1, 11):
+      self.assertEqual(True, s.push(value))
+      self.assertEqual(value, s.size())
+      self.assertEqual(False, s.has(value+1))
+	
+  def testClear(self):
+    s = stack.Stack()
+    for value in range(1, 11):
+      self.assertEqual(True, s.push(value))
+	
+    self.assertEqual(10, s.size())
+    s.clear()
+    self.assertEqual(0, s.size())
+
+if __name__ == '__main__':
+    unittest.main()
 
 
     
